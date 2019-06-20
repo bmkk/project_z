cea9a3cdd427d8fae0c9e8fb9069b3dca45df425
lmj <13433930928@163.com>
2019-06-20 23:04:26 +0800

    first up

---
+++ z/src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="head">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'head',
+  data() { 
+    return {
+
+    }
+  }
+ }
+</script>
+
+<style scoped>
+  .head{
+    color: #000;
+  }
+</style>
--- z/src/views/Home.vue
+++ z/src/views/Home.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+// @ is an alias to /src----@是/SRC的别名
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
